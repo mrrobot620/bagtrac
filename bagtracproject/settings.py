158b5d1cf23b5e0d6fd0974dff1d4bda7abfc263
Abhishek Rajput <abhishek620380@gmail.com>
2023-12-18 12:48:43 +0530

    IP

--- bagtracproject/settings.py
+++ bagtracproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-yc)-uh(n&*6+j1+b#%bqp#pv3_m@gwie68k#t6(05v&d$%=ptj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["192.168.192.156" , "localhost" , "127.0.0.1"]
+ALLOWED_HOSTS = ["192.168.192.156" , "localhost" , "127.0.0.1" , "15.207.115.205"]
 
 
 # Application definition
